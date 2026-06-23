e0f91ded43962fb85fe7464a09b65ff2f59fa320
avb <avbsoftware.co@gmail.com>
2026-06-23 12:26:39 +0530

    GTM update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,9 @@
 
 
 
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "../components/ui/Navbar";
 import EnquiryPopup from "../components/ui/EnquiryPopup";
@@ -43,7 +43,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.ultimateheights.in"),
   title: "Ultimate Heights - Only 5 Luxury Flats Left in Bhopal",
-  description: "96.3% Sold Out! Premium flats near Aura Mall, Shahpura. Book your dream home today at Ultimate Heights.",
+  description:
+    "96.3% Sold Out! Premium flats near Aura Mall, Shahpura. Book your dream home today at Ultimate Heights.",
 };
 
 export default function RootLayout({
@@ -53,7 +54,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W73ZN7LM');
+          `}
+        </Script>
+      </head>
+
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W73ZN7LM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Navbar />
         {children}
         <EnquiryPopup />
