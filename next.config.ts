9805a36667aabb417dd16c3ea287b8d00b311c70
apple <apple@apples-MacBook-Air.local>
2026-09-25 15:07:09 +0530

    url update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   trailingSlash: false, // 👈 / हटाएगा (Clean URL: /paradise)
+//   images: {
+//     unoptimized: true,
+//     qualities: [75, 90, 100],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: false, // 👈 / हटाएगा (Clean URL: /paradise)
+  trailingSlash: false, // 👈 इससे URL में आख़िर का / नहीं आएगा
   images: {
     unoptimized: true,
     qualities: [75, 90, 100],
