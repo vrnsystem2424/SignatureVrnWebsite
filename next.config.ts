0386dd4eb051077015862200627c49565b643c6b
apple <apple@apples-MacBook-Air.local>
2026-09-25 14:39:57 +0530

    macbook book update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: false,   // ✅ false कर दो — URL में / नहीं आएगा
+  trailingSlash: false,
   images: {
     unoptimized: true,
     qualities: [75, 90, 100],
